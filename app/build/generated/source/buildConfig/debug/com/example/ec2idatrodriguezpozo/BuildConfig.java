f6d9bb2e26486467cf2fff06fb0639ee936a132e
Matias <matiasrodriguezpozo14@gmail.com>
2023-07-05 17:29:08 -0500

    Agregando el APK

---
+++ app/build/generated/source/buildConfig/debug/com/example/ec2idatrodriguezpozo/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.ec2idatrodriguezpozo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.ec2idatrodriguezpozo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String MAPS_API_KEY = "AIzaSyC20jViu_dNwKWr1r-GY2gFF6eioM3304M";
+}
